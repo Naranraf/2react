c586c432f0d38da20827d0dc65be99600710fc40
Rafael Naranjo <106176753+Naranraf@users.noreply.github.com>
2023-05-07 10:28:38 -0600

    3

--- 2react/src/App.jsx
+++ 2react/src/App.jsx
@@ -1,20 +1,25 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import css from "./App.css"
+import "./App.css";
 
 function WeatherApp() {
   const [weatherData, setWeatherData] = useState(null);
-  const [latitude, setLatitude] = useState(null);
-  const [longitude, setLongitude] = useState(null);
-  const [isCelsius, setIsCelsius] = useState(true); // Nuevo estado para mantener la unidad de medida actual
-
-  console.log(longitude , latitude)
+  const [unit, setUnit] = useState("Celsius");
+  const [isNightMode, setIsNightMode] = useState(false);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
       (position) => {
-        setLatitude(position.coords.latitude.toFixed(2));
-        setLongitude(position.coords.longitude.toFixed(2));
+        const { latitude, longitude } = position.coords;
+        axios(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=e341ebcf111a23ff95961d8055d77047&units=metric`
+        )
+          .then((response) => {
+            setWeatherData(response.data);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
       },
       (error) => {
         console.log(error);
@@ -22,32 +27,56 @@ function WeatherApp() {
     );
   }, []);
 
-  useEffect(() => {
-    if (latitude && longitude) {
-      axios(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=e341ebcf111a23ff95961d8055d77047`)
-        .then((response) => {
-          setWeatherData(response.data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+  const handleUnitChange = () => {
+    if (unit === "Celsius") {
+      setUnit("Fahrenheit");
+    } else {
+      setUnit("Celsius");
     }
-  }, [latitude, longitude]);
+  };
 
-  const handleClick = () => {
-    setIsCelsius(!isCelsius);
-  }
+  const handleNightModeToggle = () => {
+    setIsNightMode(!isNightMode);
+  };
 
   return (
-    <div className="weatherCard">
+    <div className={`weatherCard ${isNightMode ? "nightMode" : ""}`}>
       {weatherData ? (
-        <div>
-          <h2>{weatherData.name}, {weatherData.sys.country}</h2>
-          <img src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`} alt="Weather Icon"/>
-          <p>{weatherData.weather[0].description}</p>
-          <p>{isCelsius ? `${weatherData.main.temp} °C` : `${(weatherData.main.temp * 9 / 5 + 32).toFixed(2)} °F`}</p> {/* Usamos el estado isCelsius para mostrar la temperatura en Celsius o Fahrenheit */}
-          <button onClick={handleClick}>Change to {isCelsius ? 'Fahrenheit' : 'Celsius'}</button> {/* Agregamos un botón para cambiar la unidad de medida */}
-        </div>
+        <>
+          <div className="header">
+            <h2>{weatherData.name}</h2>
+            
+          </div>
+          <div className="body">
+            <img
+              className="weatherIcon"
+              src={`http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`}
+              
+              alt={weatherData.weather[0].description}
+            />
+            <div className="weatherInfo">
+              <p className="weatherDescription">
+                {weatherData.weather[0].description}
+              </p>
+              <p className="temperature">
+                {unit === "Celsius"
+                  ? `${Math.round(weatherData.main.temp)}°C`
+                  : `${Math.round(
+                      (weatherData.main.temp * 9) / 5 + 32
+                    )}°F`}
+              </p>
+              <p className="humidity">
+                Humidity:<br></br>{weatherData.main.humidity}%
+              </p>
+              <button className="unitBtn" onClick={handleUnitChange}>
+              {unit}
+            </button>
+            </div>
+            <input type="toggle" className="nightModeBtn" onClick={handleNightModeToggle}>
+              {isNightMode ? "Day Mode" : "Night Mode"}
+            </input>
+          </div>
+        </>
       ) : (
         <p>Loading...</p>
       )}
